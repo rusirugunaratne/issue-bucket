fddee01e4c304f8f98e37521681d617cb06849be
rusirugunaratne <info.rusiru@gmail.com>
2024-02-16 22:39:02 +0530

    build a styled link component

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,21 +2,33 @@ import React from "react"
 import prisma from "@/prisma/client"
 import { notFound } from "next/navigation"
 import { number } from "zod"
+import { Card, Flex, Heading, Text } from "@radix-ui/themes"
+import IssueStatusBadge from "@/app/components/IssueStatusBadge"
+import ReactMarkdown from "react-markdown"
 
 interface Props {
   params: { id: string }
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-  if (typeof params.id !== "number") notFound()
-
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   })
 
   if (!issue) notFound()
 
-  return <div>{issue.title}</div>
+  return (
+    <div>
+      <Heading>{issue.title}</Heading>
+      <Flex gap='3' my='2'>
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className='prose' mt='4'>
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
+    </div>
+  )
 }
 
 export default IssueDetailPage
