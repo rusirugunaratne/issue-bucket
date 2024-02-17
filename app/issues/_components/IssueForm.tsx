0137062ab77bad780a298893559ed55d4e082486
rusirugunaratne <info.rusiru@gmail.com>
2024-02-18 02:12:00 +0530

    improve the loading experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -8,15 +8,11 @@ import { Issue } from "@prisma/client"
 import { Button, Callout, TextField } from "@radix-ui/themes"
 import axios from "axios"
 import "easymde/dist/easymde.min.css"
-import dynamic from "next/dynamic"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
 import { Controller, useForm } from "react-hook-form"
+import SimpleMdeReact from "react-simplemde-editor"
 import { z } from "zod"
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-})
 
 type IssueFormData = z.infer<typeof issueSchema>
 
@@ -67,7 +63,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
           defaultValue={issue?.description}
           control={control}
           render={({ field }) => (
-            <SimpleMDE placeholder='Description' {...field} />
+            <SimpleMdeReact placeholder='Description' {...field} />
           )}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,11 @@
-import React from "react"
+import dynamic from "next/dynamic"
 import IssueForm from "../_components/IssueForm"
+import IssueFormSkeleton from "./loading"
+
+const issueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+})
 
 const NewIssuePage = () => {
   return <IssueForm />
